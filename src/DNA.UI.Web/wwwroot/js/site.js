91be7390ac7539bad15961e8033f0dea4b005c93
PauloGaldino <44450145+PauloGaldino@users.noreply.github.com>
2020-06-04 18:26:06 -0300

    Implemetada as telas de apresentação mas com bug no carrocel

--- src/DNA.UI.Web/wwwroot/js/site.js
+++ src/DNA.UI.Web/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+
+}
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
